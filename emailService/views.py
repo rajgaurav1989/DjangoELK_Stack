3e0e3e98a0d12479021c469184fa5fc9ecfd2fc3
Raj Gaurav <rajgaurav1989.bits10@gmail.com>
2018-11-04 15:32:05 +0530

    email service ui and form validation complete

--- emailService/views.py
+++ emailService/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
+@csrf_exempt
+@login_required
 def singleEmail(request) :
-	return render_to_response("emailService/singleEmail.html",
+    if (request.method == 'POST') :
+        receiverEmail = request.POST.get('receiverEmail')
+        ccEmailIds = [x.strip() for x in request.POST.get('ccEmails').split(",") ]
+        bccEmailIds = [x.strip() for x in request.POST.get('bccEmails').split(",") ]
+        subject = request.POST.get('emailSubject')
+        message = request.POST.get('emailBody')
+        print receiverEmail+'\t'+str(ccEmailIds)+'\t'+str(bccEmailIds)+'\t'+subject+'\t'+message
+        return render_to_response("emailService/submit.html",
+                                  {},
+                                  context_instance=RequestContext(request))
+    else:
+        return render_to_response("emailService/singleEmail.html",
                                   {},
                                   context_instance=RequestContext(request))
 
+@csrf_exempt
+@login_required
 def bulkEmail(request) :
-	return render_to_response("emailService/bulkEmail.html",
+    if (request.method == 'POST') :
+        return render_to_response("emailService/submit.html",
+                                  {},
+                                  context_instance=RequestContext(request))
+    else:
+	    return render_to_response("emailService/bulkEmail.html",
                                   {},
                                   context_instance=RequestContext(request))
